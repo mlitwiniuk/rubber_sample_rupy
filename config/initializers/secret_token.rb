6f7bed710b16e1d17111c72d87883e70fc655d50
Maciej Litwiniuk <maciej@galdomedia.pl>
2011-06-21 15:36:16 +0200

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rupyrumble::Application.config.secret_token = '56a5e04ac57d8ebad35ce30846381b933f05f2e6f9ed3b04944961a1028ffc78c4c33684bf5c70365b2a7a0ee504ced325737f0d5c7b3ebd92ee08bf07799fbe'
